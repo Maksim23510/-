6e273dc318ef4eee320db7553037d9d588d3695f
Matvey <104635009+kudril@users.noreply.github.com>
2022-05-03 17:33:49 +0300

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -45,10 +45,10 @@ class TestWin(QWidget):
 	    self.h_line.addLayout(self.r_line)
 	    main_win.setLayout(self.h_line)
 #первый таймер
-	def timer_test(self):
-		global time
-		time = QTime(0, 1, 0)
-		self.timer = QTimer()
+    def timer_test(self):
+	global time
+	time = QTime(0, 1, 0)
+	self.timer = QTimer()
         self.timer.timeout.connect(self.timer1Event)
         self.timer.start(1000)
     def timer1Event(self):
@@ -59,36 +59,32 @@ class TestWin(QWidget):
         self.text_timer.setStyleSheet("color: rgb(0,0,0)")
         if time.toString("hh:mm:ss") == "00:00:00":
             self.timer.stop()
-
-
-
     def connects(self):
         self.btn_test1.clicked.connect(self.timer_test)
 
 #второй таймер
     def timer_sits(self):
-      global time
-      time = QTime(0, 0, 30)
-      self.timer = QTimer()
-      self.timer.timeout.connect(self.timer2Event)
-      self.timer.start(1500)
+        global time
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
    def timer2Event(self):
-  	global time
-      time = time.addSecs(-1)
-      self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
-      self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
-      self.text_timer.setStyleSheet("color: rgb(0,0,0)")
-      if time.toString("hh:mm:ss") == "00:00:00":
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
           self.timer.stop()
-
     def connects(self):
         self.btn_test1.clicked.connect(self.timer_sits)
 #третий таймер
-	def timer_final(self):
-      global time
-      self.timer.timeout.connect(self.timer3Event)	
-      self.timer = QTimer()
-      self.timer.start(1000)
+    def timer_final(self):
+        global time
+        self.timer.timeout.connect(self.timer3Event)	
+        self.timer = QTimer()
+        self.timer.start(1000)
     def timer3Event(self):
     	if int(time.toString("hh:mm:ss")[6:8]) >= 45:
            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
